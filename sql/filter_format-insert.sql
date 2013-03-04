048ed0991ed52714c84485616a90201679ddb9e7
William Spademan <wspademan@gmail.com>
2013-03-04 10:51:40 -0500

    fixing caps

---
+++ sql/filter_format-insert.sql
@@ -0,0 +1,3 @@
+INSERT INTO `filter_format` (`format`, `name`, `cache`, `status`, `weight`) VALUES
+('filtered_html', 'Filtered HTML', 1, 1, 0),
+('plain_text', 'Plain text', 1, 1, 10);
---
+++ sql/role_permission-insert.sql
@@ -0,0 +1,5 @@
+INSERT INTO `role_permission` (`rid`, `permission`, `module`) VALUES
+(1, 'access content', 'node'),
+(1, 'use text format filtered_html', 'filter'),
+(2, 'access content', 'node'),
+(2, 'use text format filtered_html', 'filter');
