aa902ddabb1f7745d82495ff95a117c45acd49b3
William Spademan <wspademan@gmail.com>
2013-03-10 19:43:37 -0400

    - daily cron (most) - USD atomic transaction handling (mostly untested)

--- sql/r_user_industries-make.sql
+++ sql/r_user_industries-make.sql
@@ -3,4 +3,4 @@ CREATE TABLE IF NOT EXISTS `r_user_industries` (
   `uid` int(11) unsigned NOT NULL DEFAULT '0',
   KEY `iid` (`iid`),
   KEY `uid` (`uid`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
