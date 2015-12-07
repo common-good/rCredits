de01eccca8d71e3d987ba9a2cc1e137d345aa768
Elizabeth Mitchell <elizabeth@sassafras.coop>
2015-12-07 17:02:25 -0500

    39791: Remove starter portions out of routes and controller

--- www/js/routes.js
+++ www/js/routes.js
@@ -28,26 +28,6 @@ angular.module('routes', [])
   //   }
   // })
 
-  // .state('app.playlists', {
-  //   url: '/playlists',
-  //   views: {
-  //     'menuContent': {
-  //       templateUrl: 'templates/playlists.html',
-  //       controller: 'PlaylistsCtrl'
-  //     }
-  //   }
-  // })
-
-  // .state('app.single', {
-  //   url: '/playlists/:playlistId',
-  //   views: {
-  //     'menuContent': {
-  //       templateUrl: 'templates/playlist.html',
-  //       controller: 'PlaylistCtrl'
-  //     }
-  //   }
-  // })
-
   .state('app.home', {
     url: '/home',
     views: {
