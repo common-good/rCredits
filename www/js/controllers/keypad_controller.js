4b9640f73f0107a634cedcc87ed6650fa099dbb7
Elizabeth Mitchell <elizabeth@sassafras.coop>
2015-12-16 16:52:42 -0500

    3794: Ensure remaining buttons function as expected
    - Add clear and 00 actions
    - Clean up controller

--- www/js/controllers/keypad_controller.js
+++ www/js/controllers/keypad_controller.js
@@ -1,24 +1,19 @@
-app.controller('KeyPadCtrl', function($scope, $state, $ionicLoading) {
+app.controller('KeyPadCtrl', function($scope, $state) {
   $scope.amount = 0;
 
-  $scope.keyPress = function(key) {
-    console.log(key);
-
-    // Calls another function for calculate
-    $scope.addNumber(key);
-  }
-
   $scope.addHundred = function(key) {
-    console.log("00 was clicked");
-  }
-
-  $scope.clearEntry = function(key) {
-    console.log("Clear was clicked");
+    $scope.amount = $scope.amount * 100;
   }
 
   $scope.addNumber = function(num) {
-    // console.log("Starting Amount:" + $scope.amount);
     $scope.amount = ($scope.amount * 10) + (num/100);
-    // console.log("New amount: " + $scope.amount);
+  }
+
+  $scope.clearEntry = function(key) {
+    $scope.amount = 0;
+  }
+
+  $scope.keyPress = function(key) {
+    $scope.addNumber(key);
   }
 });
