45e8278dc902f7bc1dcff100c69016164d18fddc
Sergio Milici <sergio.milici@gmail.com>
2015-12-16 17:17:58 -0300

    Added environments to config file

--- config.js
+++ config.js
@@ -1,9 +1,23 @@
 rCredits = {
 
   // @if BUILD_TARGET='development'
+  serverUrl:'https://ws.rcredits.org/pos?23',
+  version: '3.0',
+  build: 100
+  // @endif
+
+  // @if BUILD_TARGET='staging'
+  serverUrl:'https://ws.rcredits.org/pos?23',
+  version: '3.0',
+  build: 100
+  // @endif
+
+  // @if BUILD_TARGET='production'
+  serverUrl:'',
+  version: '3.0',
+  build: 100
   // @endif
 
 
-  version: '3.0',
-  build: 100
+
 };
