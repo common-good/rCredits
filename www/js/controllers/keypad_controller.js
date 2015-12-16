03b2c8aad08067b9ccadff90be35e62b82632ad8
Elizabeth Mitchell <elizabeth@sassafras.coop>
2015-12-16 16:40:58 -0500

    3794: Add key press functions to buttons

--- www/js/controllers/keypad_controller.js
+++ www/js/controllers/keypad_controller.js
@@ -1,2 +1,15 @@
-app.controller('KeyPadCtrl', function($scope, $state, $ionicLoading, $ionicPopup) {
+app.controller('KeyPadCtrl', function($scope, $state, $ionicLoading) {
+  $scope.keyPress = function(key) {
+    console.log(key);
+
+    // Calls another function for calculate
+  }
+
+  $scope.addHundred = function(key) {
+    console.log("00 was clicked");
+  }
+
+  $scope.clearEntry = function(key) {
+    console.log("Clear was clicked");
+  }
 });
