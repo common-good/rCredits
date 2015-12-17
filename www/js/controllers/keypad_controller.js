4b078382faa41a7379e6873de02d1465198b8cc2
Elizabeth Mitchell <elizabeth@sassafras.coop>
2015-12-17 13:04:17 -0500

    3794: Simplify keypad functions

--- www/js/controllers/keypad_controller.js
+++ www/js/controllers/keypad_controller.js
@@ -1,19 +1,15 @@
 app.controller('KeyPadCtrl', function($scope, $state) {
   $scope.amount = 0;
 
-  $scope.addHundred = function(key) {
+  $scope.addHundred = function(num) {
     $scope.amount = $scope.amount * 100;
   }
 
   $scope.addNumber = function(num) {
-    $scope.amount = ($scope.amount * 10) + (num/100);
+    $scope.amount = ($scope.amount * 10) + (num / 100);
   }
 
   $scope.clearEntry = function(key) {
     $scope.amount = 0;
-  }
-
-  $scope.keyPress = function(key) {
-    $scope.addNumber(key);
   }
 });
