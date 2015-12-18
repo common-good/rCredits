fee28f5b4afda660ba4c625de07fcc748e890b7d
Elizabeth Mitchell <elizabeth@sassafras.coop>
2015-12-18 08:49:52 -0500

    3794: Tweak keypad controller functions

--- www/js/controllers/keypad_controller.js
+++ www/js/controllers/keypad_controller.js
@@ -2,20 +2,18 @@ app.controller('KeyPadCtrl', function($scope, $state) {
   $scope.amount = 0;
 
   $scope.addHundred = function(num) {
-    value = $scope.amount * 100;
-    checkValue(value);
+    setEntry($scope.amount * 100);
   }
 
   $scope.addNumber = function(num) {
-    value = ($scope.amount * 10) + (num / 100);
-    checkValue(value);
+    setEntry(($scope.amount * 10) + (num / 100));
   }
 
-  $scope.clearEntry = function(key) {
+  $scope.clearEntry = function() {
     $scope.amount = 0;
   }
 
-  var checkValue = function(num) {
+  var setEntry = function(num) {
     if (num < 1000000) {
       $scope.amount = num;
     }
