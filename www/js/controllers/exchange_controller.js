5f5fad957afdc6ed0d04eb543d7a5efad4f7f047
Sergio Milici <sergio.milici@gmail.com>
2016-02-08 19:01:13 -0300

    Added keyboard, style and binding

--- www/js/controllers/exchange_controller.js
+++ www/js/controllers/exchange_controller.js
@@ -2,10 +2,22 @@
 
   'use strict';
 
-  app.controller('ExchangeCtrl', function(ExchangeService) {
+  app.controller('ExchangeCtrl', function($scope, ExchangeService, $translate) {
     var self = this;
 
+    $scope.amount = 0;
 
+
+    this.init = function() {
+      $translate('exchange_includes_fee').then(function(msg) {
+        self.paymentFeeTitle = msg;
+      });
+    };
+
+    this.doExchange = function() {
+    };
+
+    this.init();
   });
 
 })(app);
