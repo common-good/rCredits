900bbb28942774c73e438b3a59f35926d3ed2dbe
Sergio Milici <sergio.milici@gmail.com>
2016-02-23 11:22:46 -0300

    Added offline message to all pages

--- www/js/directives/offline_header.js
+++ www/js/directives/offline_header.js
@@ -7,31 +7,43 @@
       templateUrl: 'templates/offline_header.html',
       bindToController: true,
       controllerAs: 'offlCtrl',
-      controller: function($scope) {
+      link: function(scope) {
         var self = this, ionContent;
 
-        this.init = function() {
+        var init = function() {
           ionContent = document.getElementsByTagName("ion-content")[0];
+          scope.ionContent = ionContent;
+          scope.$watch(function() {
+            return NetworkService.isOffline()
+          }, function(newValue, oldValue) {
+            if (newValue) {
+              $timeout(function() {
+                ionContent.addClassName('has-subheader');
+              });
+            } else {
+              $timeout(function() {
+                ionContent.removeClassName('has-subheader');
+              });
+            }
+          });
         };
 
-        $scope.$watch(function() {
-          return NetworkService.isOffline()
-        }, function(newValue, oldValue) {
-          if (newValue) {
-            $timeout(function() {
-              ionContent.addClassName('has-subheader');
-            });
-          } else {
-            $timeout(function() {
-              ionContent.removeClassName('has-subheader');
-            });
-          }
-        });
 
         $timeout(function() {
-          self.init();
-        });
+          init();
+        },1000);
+      },
+      controller: function($scope) {
 
+        this.isOffline = function() {
+          return NetworkService.isOffline();
+        };
+
+        $scope.$on('$destroy', function() {
+          if ($scope.ionContent) {
+            $scope.ionContent.removeClassName('has-subheader');
+          }
+        });
       }
 
     };
--- www/js/services/network_service.js
+++ www/js/services/network_service.js
@@ -17,28 +17,20 @@
       if (navigator && navigator.connection) {
         this.connectionOnline = navigator.connection.type !== 'none';
       }
-      //console.log("Connection is online: ", !this.isOffline());
-
-      $rootScope.isOffline = function() {
-        return self.isOffline();
-      }
-
     };
 
     NetworkService.prototype.onOffline = function() {
-      console.warn("OFFLINE");
       this.connectionOnline = false;
       $rootScope.$apply();
     };
 
     NetworkService.prototype.onOnline = function() {
-      console.info("ONLINE");
       this.connectionOnline = true;
       $rootScope.$apply();
     };
 
     NetworkService.prototype.isOffline = function() {
-      return !this.connectionOnline;
+      return !!this.connectionOnline;
     };
 
 
