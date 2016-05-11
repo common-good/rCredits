7fc3130de7862bdff7593264dded0e45dddfd685
Aaron E-J <the@otherrealm.org>
2016-05-11 12:06:57 -0400

    rCredits-mobile - mostly formating changes

--- www/js/controllers/customer_menu_controller.js
+++ www/js/controllers/customer_menu_controller.js
@@ -1,85 +1,73 @@
-app.controller('CustomerMenuCtrl', function($scope, $state, $ionicLoading, UserService, $ionicHistory, NotificationService, CashierModeService, PermissionService, SelfServiceMode) {
-
-  $scope.customer = UserService.currentCustomer();
-
-  $scope.showBalance = function() {
-    if ($scope.customer.balanceSecret) {
-      NotificationService.showAlert('balanceIsSecret');
-    } else {
-      NotificationService.showAlert({
-        scope: $scope,
-        title: 'customerBalance',
-        templateUrl: 'templates/customer-balance.html'
-      });
-    }
-  };
-
-  $scope.hideLoading = function() {
-    $ionicLoading.hide();
-  };
-
-  $scope.$on('$destroy', function() {
-    $scope.customer = null;
-  });
-
-  $scope.openCharge = function() {
-    var chargeFn = function() {
-      $state.go('app.transaction', {'transactionType': 'charge'});
-    };
-
-    if (CashierModeService.canCharge()) {
-      chargeFn();
-    } else {
-      executeAction(chargeFn);
-    }
-  };
-
-  $scope.openRefund = function() {
-    var refundFn = function() {
-      $state.go('app.transaction', {'transactionType': 'refund'});
-    };
-
-    if (CashierModeService.canRefund()) {
-      refundFn();
-    } else {
-      NotificationService.showAlert({title: 'action_not_enabled'});
-    }
-  };
-
-  $scope.openExchange = function() {
-    var exchangeFn = function() {
-      $state.go('app.transaction_select_exchange')
-    };
-
-    if (CashierModeService.canExchange()) {
-      exchangeFn();
-    } else {
-      NotificationService.showAlert({title: 'action_not_enabled'});
-    }
-  };
-
-  var executeAction = function(fn) {
-    NotificationService.showConfirm({
-      title: 'cashier_permission',
-      subTitle: "",
-      okText: "scanIn",
-      cancelText: "cancel"
-    }, {}).then(function(res) {
-      if (res) {
-        return PermissionService.authorizeSeller()
-          .then(function(authResult) {
-            if (!authResult) {
-              NotificationService.showAlert({title: 'cashier_permission_rejected'});
-              return;
-            }
-            fn();
-          });
-      }
-    });
-  };
-
-  $scope.isSelfServiceEnabled = function() {
-    return SelfServiceMode.isActive();
-  };
+app.controller('CustomerMenuCtrl', function ($scope, $state, $ionicLoading, UserService, $ionicHistory, NotificationService, CashierModeService, PermissionService, SelfServiceMode) {
+	$scope.customer = UserService.currentCustomer();
+	$scope.showBalance = function () {
+		if ($scope.customer.balanceSecret) {
+			NotificationService.showAlert('balanceIsSecret');
+		} else {
+			NotificationService.showAlert({
+				scope: $scope,
+				title: 'customerBalance',
+				templateUrl: 'templates/customer-balance.html'
+			});
+		}
+	};
+	$scope.hideLoading = function () {
+		$ionicLoading.hide();
+	};
+	$scope.$on('$destroy', function () {
+		$scope.customer = null;
+	});
+	$scope.openCharge = function () {
+		var chargeFn = function () {
+			$state.go('app.transaction', {'transactionType': 'charge'});
+		};
+		if (CashierModeService.canCharge()) {
+			chargeFn();
+		} else {
+			executeAction(chargeFn);
+		}
+	};
+	$scope.openRefund = function () {
+		var refundFn = function () {
+			$state.go('app.transaction', {'transactionType': 'refund'});
+		};
+		if (CashierModeService.canRefund()) {
+			refundFn();
+		} else {
+			NotificationService.showAlert({title: 'action_not_enabled'});
+		}
+	};
+	$scope.openExchange = function () {
+		var exchangeFn = function () {
+			$state.go('app.transaction_select_exchange');
+		};
+		if (CashierModeService.canExchange()) {
+			exchangeFn();
+		} else {
+			NotificationService.showAlert({title: 'action_not_enabled'});
+		}
+	};
+	var executeAction = function (fn) {
+		NotificationService.showConfirm({
+			title: 'cashier_permission',
+			subTitle: "",
+			okText: "scanIn",
+			cancelText: "cancel"
+		}, {}).then(function (res) {
+			if (res) {
+				return PermissionService.authorizeSeller()
+					.then(function (authResult) {
+						if (!authResult) {
+							NotificationService.showAlert({title: 'cashier_permission_rejected'});
+							return;
+						}
+						fn();
+					});
+			}
+		});
+	};
+	$scope.isSelfServiceEnabled = function () {
+		return SelfServiceMode.isActive();
+	};
 
 });
--- www/js/controllers/preferences_controller.js
+++ www/js/controllers/preferences_controller.js
@@ -1,36 +1,27 @@
-app.controller('PreferencesCtrl', function($scope, $state, UserService, PreferenceService, $ionicLoading, NotificationService) {
-
-
-  var preferences = PreferenceService.getAll();
-  $scope.preferences = preferences;
-
-  $scope.saveItems = function() {
-    PreferenceService.savePreferences(preferences);
-  };
-
-  $scope.cashierModePref = PreferenceService.getCashierModePref();
-  $scope.cashierCanModePref = PreferenceService.getCashierCanPref();
-
-
-  $scope.doAction = function(pref) {
-    if (pref.id === 'forget_company') {
-      $ionicLoading.show();
-
-      UserService.logout()
-        .then(function() {
-          $state.go("app.login");
-        })
-        .catch(function(errorMsg) {
-          NotificationService.showAlert({title: "error", template: errorMsg});
-        })
-        .finally(function() {
-          $ionicLoading.hide();
-        });
-    }
-  };
-
-  $scope.openLink = function(url) {
-    window.open(encodeURI(url), '_system');
-  };
-
+app.controller('PreferencesCtrl', function ($scope, $state, UserService, PreferenceService, $ionicLoading, NotificationService) {
+	var preferences = PreferenceService.getAll();
+	$scope.preferences = preferences;
+	$scope.saveItems = function () {
+		PreferenceService.savePreferences(preferences);
+	};
+	$scope.cashierModePref = PreferenceService.getCashierModePref();
+	$scope.cashierCanModePref = PreferenceService.getCashierCanPref();
+	$scope.doAction = function (pref) {
+		if (pref.id === 'forget_company') {
+			$ionicLoading.show();
+			UserService.logout()
+				.then(function () {
+					$state.go("app.login");
+				})
+				.catch(function (errorMsg) {
+					NotificationService.showAlert({title: "error", template: errorMsg});
+				})
+				.finally(function () {
+					$ionicLoading.hide();
+				});
+		}
+	};
+	$scope.openLink = function (url) {
+		window.open(encodeURI(url), '_system');
+	};
 });
